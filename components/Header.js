f511e615162e8a0c836319b4fa0a0e20f056e108
nischay <nischayreddy.t@gmail.com>
2025-07-31 17:12:10 +0530

    Foodievery logo shortcut

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 // Remove the import and use the public folder for the image
 import { useState } from "react";
+import Body from "./Body";
 const Header = () => {
   const [LoginBtn, setLoginBtn] = useState("Login");
   return (
     <div className="header">
-      <div className="logo">FOODIEVERY </div>
+      <div
+        className="logo"
+        onClick={() => {
+          window.location.href = "/Body";
+        }}
+      >
+        FOODIEVERY{" "}
+      </div>
       <div className="NavItems">
         <ul>
           <li>HOME</li>
